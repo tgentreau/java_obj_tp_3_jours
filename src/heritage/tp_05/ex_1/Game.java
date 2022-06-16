00e060aa6ea2ba71276b873ba4e790a0b73d1fb7
tgentreau <gentreau.thomas@gmail.com>
2022-06-16 11:55:03 +0200

    tp-jeu ex1 et 2

--- src/heritage/tp_05/ex_1/Game.java
+++ src/heritage/tp_05/ex_1/Game.java
@@ -4,32 +4,125 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int randomInt = random.nextInt(100) + 1;
-        int essais = 10;
-        Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+    private int randomIntFacile = random.nextInt(100) + 1;
+    private int randomIntMoyen = random.nextInt(1000) + 1;
+    private int essais = 10;
+
+    public int getRandomIntFacile() {
+        return randomIntFacile;
+    }
+
+    public int getRandomIntMoyen() {
+        return randomIntMoyen;
+    }
+
+    public int getEssais() {
+        return essais;
+    }
+
+    public void setUp() {
+        System.out.println("Veuillez choisir un niveau de difficulté");
+        System.out.println("1- Facile");
+        System.out.println("2- Moyen");
+        System.out.println("3- Difficile");
+        int choix = scanner.nextInt();
+        if(choix == 1) {
+            facile();
+        } else if(choix == 2) {
+            moyen();
+        } else if(choix == 3) {
+            difficile();
+        } else {
+            System.out.println("Mauvaise entrée");
+        }
+    }
+
+    public void facile() {
         System.out.println("New game");
         System.out.println("Veuillez entrer un nombre aléatoire entre 1 et 100");
         System.out.println("Vous avez " + essais + " essais");
         while (essais != 0) {
             int result = scanner.nextInt();
-            if(result < randomInt) {
+            if(result < randomIntFacile) {
                 System.out.println("Trop petit");
                 essais--;
                 System.out.println("Essais restant: " + essais);
-            } else if(result > randomInt) {
+            } else if(result > randomIntFacile) {
                 System.out.println("Trop grand");
                 essais--;
                 System.out.println("Essais restant: " + essais);
-            } else if(result == randomInt) {
+            } else if(result == randomIntFacile) {
                 System.out.println("Vous avez gagné, Bravo !");
-                scanner.close();
+                restart();
             }
             if(essais == 0) {
                 System.out.println("Game over");
+                restart();
             }
         }
-
     }
+
+    public void moyen() {
+        System.out.println("New game");
+        System.out.println("Veuillez entrer un nombre aléatoire entre 1 et 1000");
+        System.out.println("Vous avez " + essais + " essais");
+        while (essais != 0) {
+            int result = scanner.nextInt();
+            if(result < randomIntMoyen) {
+                System.out.println("Trop petit");
+                essais--;
+                System.out.println("Essais restant: " + essais);
+            } else if(result > randomIntMoyen) {
+                System.out.println("Trop grand");
+                essais--;
+                System.out.println("Essais restant: " + essais);
+            } else if(result == randomIntMoyen) {
+                System.out.println("Vous avez gagné, Bravo !");
+                restart();
+            }
+            if(essais == 0) {
+                System.out.println("Game over");
+                restart();
+            }
+        }
+    }
+
+    public void difficile() {
+        System.out.println("New game");
+        System.out.println("Veuillez entrer un nombre aléatoire entre 1 et 1000");
+        System.out.println("Vous avez " + essais + " essais");
+        while (essais != 0) {
+            int result = scanner.nextInt();
+            if(result < randomIntMoyen || result > randomIntMoyen) {
+                System.out.println("Raté");
+                essais--;
+                System.out.println("Essais restant: " + essais);
+            } else if(result == randomIntMoyen) {
+                System.out.println("Vous avez gagné, Bravo !");
+                restart();
+            }
+            if(essais == 0) {
+                System.out.println("Game over");
+                restart();
+            }
+        }
+    }
+
+    public void restart () {
+        System.out.println("Voulez-vous relancer le jeu ?");
+        System.out.println("1- Oui");
+        System.out.println("2- Non");
+        int restart = scanner.nextInt();
+        if(restart == 1) {
+            essais = 10;
+            setUp();
+        } else if (restart == 2) {
+            System.exit(0);
+        } else {
+            System.out.println("Mauvaise entrée");
+        }
+    }
+
 }
---
+++ src/heritage/tp_05/ex_1/TestGame.java
@@ -0,0 +1,8 @@
+package heritage.tp_05.ex_1;
+
+public class TestGame {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.setUp();
+    }
+}
